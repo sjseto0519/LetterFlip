9f4f281c403d026b20484fb49ed964823710e2cd
Scott Seto <scott.seto@outlook.com>
2023-10-25 12:48:17 -0400

    Update sort strategy.

--- LetterFlip.Frontend/src/utils/predefined-order-sort-strategy.ts
+++ LetterFlip.Frontend/src/utils/predefined-order-sort-strategy.ts
@@ -3,6 +3,10 @@ import { ISortStrategy } from "./i-sort-strategy";
 
 export class PredefinedOrderSortStrategy implements ISortStrategy {
     sort(subscribers: ISubscriberContainer[], predefinedOrder: string[]): ISubscriberContainer[] {
+        if (predefinedOrder.length === 0)
+        {
+            return subscribers;
+        }
         return subscribers.slice().sort((a, b) => {
             const indexA = predefinedOrder.indexOf(a.id);
             const indexB = predefinedOrder.indexOf(b.id);
